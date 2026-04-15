d6bbf9eddad93682825f911402447d85d43c9afc
senpgalg <senpgalg@gmail.com>
2026-04-15 17:24:25 +0900

    足し算と引き算を追加

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ assert() {
     cc -o tmp tmp.s
     ./tmp
     actual="$?"
-    
+
     if [ "$actual" = "$expected" ]; then
         echo "$input => $actual"
     else
@@ -19,5 +19,6 @@ assert() {
 
 assert 0 0
 assert 42 42
+assert 21 "5+20-4"
 
 echo OK
