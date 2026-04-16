88eff83735836ade22ab9272cb0c24053fd067fe
senpgalg <senpgalg@gmail.com>
2026-04-16 16:03:19 +0900

    9cc.c を複数のファイルに分けた

---
+++ 9cc.h
@@ -0,0 +1,71 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//
+// tokenize.c
+//
+
+// トークンの種類
+typedef enum {
+    TK_RESERVED,    // 記号
+    TK_NUM,         // 整数トークン
+    TK_EOF,         // 入力の終わりを表すトークン
+} TokenKind;
+
+// トークンの型
+typedef struct Token Token;
+struct Token {
+    TokenKind kind; // トークンの型
+    Token *next;    // 次の入力トークン
+    int val;        // kind が TK_NUM の場合、その数値
+    char *str;      // トークン文字列
+    int len;        // トークンの長さ
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+long expect_number(void);
+bool at_eof(void);
+Token *tokenize(void);
+
+extern char *user_input;
+extern Token *token;
+
+//
+// parse.c
+//
+
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_EQ,  // ==
+    ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
+    ND_NUM, // Integer
+} NodeKind;
+
+// AST node Type
+typedef struct Node Node;
+struct Node {
+    NodeKind kind;  // Node kind
+    Node *lhs;      // Left-hand side
+    Node *rhs;      // Right-hand side
+    long val;        // Used if kind == ND_NUM
+};
+
+Node *expr(void);
+
+//
+// codegen.c
+//
+
+void codegen(Node *node);
